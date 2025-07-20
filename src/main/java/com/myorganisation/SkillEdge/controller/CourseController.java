6276b9b5dafdad9495f1fd7909cef5825d7b1d8c
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-07-20 18:09:16 +0530

    Add 'api' prefix in all endpoints

--- src/main/java/com/myorganisation/SkillEdge/controller/CourseController.java
+++ src/main/java/com/myorganisation/SkillEdge/controller/CourseController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/course")
+@RequestMapping("/api/course")
 public class CourseController {
 
     @Autowired
