b04640595702c005b5a5722ec81d29319a6c5fd7
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-08-24 17:49:21 +0530

    Add global exception handler

---
+++ src/main/java/com/myorganisation/SkillEdge/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.myorganisation.SkillEdge.exception;
+
+import com.myorganisation.SkillEdge.dto.GenericResponseDto;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(StudentNotFoundException.class)
+    public ResponseEntity<GenericResponseDto> handleStudentNotFoundException(StudentNotFoundException e) {
+        GenericResponseDto genericResponseDto = new GenericResponseDto();
+        genericResponseDto.setSuccess(false);
+        genericResponseDto.setMessage(e.getMessage());
+
+        return new ResponseEntity<>(genericResponseDto, HttpStatusCode.valueOf(404));
+    }
+}
