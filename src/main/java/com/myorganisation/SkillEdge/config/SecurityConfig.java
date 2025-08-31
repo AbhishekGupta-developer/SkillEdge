f2becef191e2fd74041bcced2e196808cbfa5c60
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-08-31 18:28:54 +0530

    Add SecurityConfig

---
+++ src/main/java/com/myorganisation/SkillEdge/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.myorganisation.SkillEdge.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(
+                        auth ->
+                                auth
+                                        .requestMatchers("/api/user", "/api/user/**").permitAll()
+                                        .requestMatchers("/api/**").authenticated()
+                                        .anyRequest().authenticated()
+                )
+                .formLogin(form ->form.permitAll())
+                .logout(logout -> logout.permitAll());
+
+        return httpSecurity.build();
+    }
+}
