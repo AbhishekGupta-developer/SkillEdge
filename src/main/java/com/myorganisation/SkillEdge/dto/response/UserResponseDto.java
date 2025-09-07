ba526fc0c0f775c165de3aee1c0ae08004df762d
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-09-07 20:46:13 +0530

    Add UserRole in UserResponseDto

--- src/main/java/com/myorganisation/SkillEdge/dto/response/UserResponseDto.java
+++ src/main/java/com/myorganisation/SkillEdge/dto/response/UserResponseDto.java
@@ -1,9 +1,11 @@
 package com.myorganisation.SkillEdge.dto.response;
 
+import com.myorganisation.SkillEdge.model.enums.UserRole;
 import lombok.Data;
 
 @Data
 public class UserResponseDto {
     private Long id;
     private String username;
+    private UserRole role;
 }
