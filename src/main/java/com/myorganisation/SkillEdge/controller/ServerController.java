a43e19a7bcfa3f59a5892840564057faf36a7281
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-09-07 17:56:54 +0530

    Update ServerController to return ServerStatusResponseDto

--- src/main/java/com/myorganisation/SkillEdge/controller/ServerController.java
+++ src/main/java/com/myorganisation/SkillEdge/controller/ServerController.java
@@ -1,15 +1,31 @@
 package com.myorganisation.SkillEdge.controller;
 
+import com.myorganisation.SkillEdge.dto.response.ServerStatusResponseDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
 
 @RestController
 @RequestMapping("/api")
 public class ServerController {
 
+    private final Instant serverStartTime;
+    private final String applicationName;
+
+    public ServerController(@Value("${spring.application.name}") String applicationName) {
+        this.serverStartTime = Instant.now();
+        this.applicationName = applicationName;
+    }
+
     @GetMapping
-    public String serverStatus() {
-        return "Server is live!";
+    public ResponseEntity<ServerStatusResponseDto> checkServerStatus() {
+        ServerStatusResponseDto serverStatusResponseDto = new ServerStatusResponseDto(serverStartTime, applicationName);
+
+        return new ResponseEntity<>(serverStatusResponseDto, HttpStatusCode.valueOf(200));
     }
 }
