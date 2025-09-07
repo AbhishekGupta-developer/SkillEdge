3cab0f075154a489537aa5a47cf09357c34c3cab
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-09-07 20:43:43 +0530

    Replace 'formLogin' with 'httpBasic' and add PasswordEncoder and AuthenticationManager

--- src/main/java/com/myorganisation/SkillEdge/config/SecurityConfig.java
+++ src/main/java/com/myorganisation/SkillEdge/config/SecurityConfig.java
@@ -2,8 +2,16 @@ package com.myorganisation.SkillEdge.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
@@ -20,9 +28,22 @@ public class SecurityConfig {
                                         .requestMatchers("/api/**").authenticated()
                                         .anyRequest().authenticated()
                 )
-                .formLogin(form ->form.permitAll())
-                .logout(logout -> logout.permitAll());
+                .httpBasic(withDefaults());
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(daoAuthenticationProvider);
     }
 }
