a4f9f4a55e158ce0a5d50d5c2b4bcf8bbfc1a7d2
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-09-07 20:45:41 +0530

    Add UserRole in UserRequestDto

--- src/main/java/com/myorganisation/SkillEdge/dto/request/UserRequestDto.java
+++ src/main/java/com/myorganisation/SkillEdge/dto/request/UserRequestDto.java
@@ -1,9 +1,11 @@
 package com.myorganisation.SkillEdge.dto.request;
 
+import com.myorganisation.SkillEdge.model.enums.UserRole;
 import lombok.Data;
 
 @Data
 public class UserRequestDto {
     private String username;
+    private UserRole role;
     private String password;
 }
