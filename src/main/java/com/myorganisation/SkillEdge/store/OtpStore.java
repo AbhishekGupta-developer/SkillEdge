cf94fc2973d70b87aa3f1eea4f106499ee8be057
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-11-16 20:28:17 +0530

    Add OtpStore

---
+++ src/main/java/com/myorganisation/SkillEdge/store/OtpStore.java
@@ -0,0 +1,21 @@
+package com.myorganisation.SkillEdge.store;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class OtpStore {
+
+    private static final ConcurrentHashMap<String, String> otpMap = new ConcurrentHashMap<>();
+
+    public static void storeOtp(String email, String otp) {
+        otpMap.put(email, otp);
+    }
+
+    public static String getOtp(String email) {
+        return otpMap.get(email);
+    }
+
+    public static void clearOtp(String email) {
+        otpMap.remove(email);
+    }
+
+}
